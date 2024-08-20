fe8b18849ff195bd61b27390f1ef455a86d37ff3
Matías <matias@MacBook-Pro-de-Matias.local>
2024-08-19 21:41:23 -0400

    refactor: replace default font-family with Inter font

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: { sans: ['Inter var', ...defaultTheme.fontFamily.sans], }
+		},
+	},
+	plugins: [],
+}
