a1aeb68ec1164011e0a4f85aaeddb003d85fc5b0
Matías <matiasmed.14@gmail.com>
2024-08-20 17:05:20 -0400

    refactor: replaced rsms.me inter font with fontsource local font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			fontFamily: { sans: ['Inter var', ...defaultTheme.fontFamily.sans], }
-		},
+		fontFamily: { sans: '"Inter variable", sans-serif', }
 	},
 	plugins: [],
 }
