8ae8d0b372016a7d47af0eec1da82af9e6780646
Matías <matiasmed.14@gmail.com>
2024-08-21 01:01:24 -0400

    feat(header): first version of landing header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,30 @@
+const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		fontFamily: { sans: '"Inter variable", sans-serif', }
+		fontFamily: { sans: '"Inter variable", sans-serif', },
+		colors: {
+			...colors,
+			'blue': {
+				...colors.blue,
+				50: '#E7F1F3', // Titles
+				100: '#E6EDF3', // Navbar links
+				300: '#B0C7CB', // Base text
+				500: '#868F91', // Secondary text
+				950: '#142733', // Blue background
+			},
+			'gray': {
+				...colors.gray,
+				600: '#868F91', // Subtitles
+			},
+			'slate': {
+				...colors.slate,
+				900: '#121718', // Slate background
+			},
+		},
 	},
 	plugins: [],
 }
