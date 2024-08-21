34ef81412e66ff46f2a15154e11781cb89d8cc93
Matías Medina Garrido <matiasmed.14@gmail.com>
2024-08-21 18:20:58 -0400

    build: add flowbite package

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,30 @@
-const colors = require('tailwindcss/colors')
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		"./node_modules/flowbite/**/*.js"
+	],
 	theme: {
 		fontFamily: { sans: '"Inter variable", sans-serif', },
-		colors: {
-			...colors,
-			'blue': {
-				...colors.blue,
-				50: '#E7F1F3', // Titles
-				100: '#E6EDF3', // Navbar links
-				300: '#B0C7CB', // Base text
-				500: '#868F91', // Secondary text
-				950: '#142733', // Blue background
+		extend: {
+			colors: {
+				'blue': {
+					50: '#E7F1F3', // Titles
+					100: '#E6EDF3', // Navbar links
+					300: '#B0C7CB', // Base text
+					500: '#868F91', // Secondary text
+					950: '#142733', // Blue background
+				},
+				'gray': {
+					600: '#868F91', // Subtitles
+				},
+				'slate': {
+					900: '#121718', // Slate background
+				},
 			},
-			'gray': {
-				...colors.gray,
-				600: '#868F91', // Subtitles
-			},
-			'slate': {
-				...colors.slate,
-				900: '#121718', // Slate background
-			},
-		},
+		}
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
