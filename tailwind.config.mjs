488789484a9bdc83a70eccbd39e4c8f5c87c364a
Matías Medina Garrido <matiasmed.14@gmail.com>
2024-08-23 20:42:19 -0400

    feat(experience): first version of experience section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,7 +12,7 @@ export default {
 					50: '#E7F1F3', // Titles
 					100: '#E6EDF3', // Navbar links
 					300: '#B0C7CB', // Base text
-					600: '#33A0B3', // Secondary text
+					600: '#33A0B3', // Primary color
 					950: '#142733', // Blue background
 				},
 				'gray': {
@@ -25,6 +25,7 @@ export default {
 			},
 			animation: {
 				blob: "blob 6s infinite",
+				'background-scroll': 'background-scroll 3s linear infinite',
 			},
 			keyframes: {
 				blob: {
@@ -32,7 +33,7 @@ export default {
 						transform: "translate(0px, 0px)",
 					},
 					"33%": {
-						transform: "translate(30px, -50px)",
+						transform: "translate(90px, -50px)",
 					},
 					"66%": {
 						transform: "translate(-20px, 20px)",
@@ -40,6 +41,11 @@ export default {
 					"100%": {
 						transform: "tranlate(0px, 0px)",
 					},
+				},
+				'background-scroll': {
+					'0%': { backgroundPosition: '0% 50%' },
+					'50%': { backgroundPosition: '100% 50%' },
+					'100%': { backgroundPosition: '0% 50%' },
 				},
 			},
 		}
