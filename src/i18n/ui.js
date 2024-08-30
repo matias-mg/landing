a9473d75ccdc42303fc0678b326849d9c5bf0860
Matías Medina Garrido <matiasmed.14@gmail.com>
2024-08-29 20:20:37 -0400

    feat(header): added language dropdown i18n functionality

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -8,15 +8,30 @@ export const defaultLang = 'en';
 
 export const ui = {
     en: {
+        // Site translations
         'site.title': 'Matías Medina - React Web Developer and frontend expert',
         'site.description': 'Matías Medina, specialized on web development with 4 years of experience in React and team leadership skills.',
+        // Header translations
+        "header.about-me": "About me",
+        "header.experience": "Experience",
+        "header.technologies": "Technologies",
     },
     es: {
+        // Site translations
         'site.title': 'Matías Medina - Desarrollador Web React y experto en frontend',
         'site.description': 'Matías Medina, especializado en desarrollo web con 4 años de experiencia en React y habilidades de liderazgo de equipo.',
+        // Header translations
+        "header.about-me": "Sobre mí",
+        "header.experience": "Experiencia",
+        "header.technologies": "Tecnologías",
     },
     pt: {
+        // Site translations
         'site.title': 'Matías Medina - Desenvolvedor Web React e especialista em frontend',
         'site.description': 'Matías Medina, especializado em desenvolvimento web com 4 anos de experiência em React e habilidades de liderança de equipe.',
+        // Header translations
+        "header.about-me": "Sobre mim",
+        "header.experience": "Experiência",
+        "header.technologies": "Tecnologias",
     },
 };
