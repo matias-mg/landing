4653870d80784998b58bfca5d537bf8060a08918
Matías Medina Garrido <matiasmed.14@gmail.com>
2024-08-30 00:06:55 -0400

    build: add astro-robots-txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://matiasm.com",
-  integrations: [tailwind()],
+  integrations: [tailwind(), robotsTxt()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es", "pt"],
